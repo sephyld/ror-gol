736a8f2b4b3da4554a21e6f5c7c6fd8b3bae2bef
Giuseppe Buono <giuseppe.buono4@gmail.com>
2024-11-08 22:13:18 +0100

    regenerating keys each time, not produd of myself

--- start-docker-prodlike.sh
+++ start-docker-prodlike.sh
@@ -1,6 +1,12 @@
 #! /usr/bin/bash
 
-docker build -t sephyld/ror-gol .
 WD=`pwd`
+
+rm $WD/config/credentials.yml.enc
+$WD/bin/rails credentials:edit
+
+docker build -t sephyld/ror-gol .
+
 KEY=`cat "$WD/config/master.key"`
+
 docker run -p 80:80 -e RAILS_MASTER_KEY="$KEY" sephyld/ror-gol
