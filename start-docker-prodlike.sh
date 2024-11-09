c1e82329f34297b6c56e780e58dbbc5cc0342c69
Giuseppe Buono <giuseppe.buono4@gmail.com>
2024-11-09 14:48:19 +0100

    defaulting to use ssl in production environment

--- start-docker-prodlike.sh
+++ start-docker-prodlike.sh
@@ -9,4 +9,4 @@ docker build -t sephyld/ror-gol .
 
 KEY=`cat "$WD/config/master.key"`
 
-docker run -p 80:80 -e RAILS_MASTER_KEY="$KEY" sephyld/ror-gol
+docker run -p 80:80 -e RAILS_MASTER_KEY="$KEY" -e SSL_FORCE="false" sephyld/ror-gol
